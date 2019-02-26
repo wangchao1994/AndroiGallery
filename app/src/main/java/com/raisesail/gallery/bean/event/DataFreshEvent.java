5adeff6fa6aedf98f2b3d2a305ea46694d71dffa
wangchao <wchao0829@163.com>
2019-02-26 16:30:59 +0800

    first commit

---
+++ app/src/main/java/com/raisesail/gallery/bean/event/DataFreshEvent.java
@@ -0,0 +1,8 @@
+package com.raisesail.gallery.bean.event;
+
+public class DataFreshEvent {
+    public boolean isRefresh;
+    public DataFreshEvent(boolean mRefresh){
+        isRefresh = mRefresh;
+    }
+}
